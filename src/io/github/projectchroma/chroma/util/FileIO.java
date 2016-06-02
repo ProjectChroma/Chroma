a222e12bc41bbc5d8ecf9a554c93ae5de707b691
Ian W <ian.uu12@gmail.com>
2016-06-02 10:01:33 -0700

    Make screenshots more efficient (Fixes #13)

--- src/io/github/projectchroma/chroma/util/FileIO.java
+++ src/io/github/projectchroma/chroma/util/FileIO.java
@@ -1,6 +1,8 @@
 package io.github.projectchroma.chroma.util;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
@@ -10,18 +12,26 @@ import io.github.projectchroma.chroma.settings.Settings;
 
 public class FileIO{
 	public static final File SCREENSHOT_DIR = new File("screenshots");
+	private static int screenshotIndex = 0;
 	public static void init(){
 		Settings.SETTINGS_DIR.mkdirs();
 		SCREENSHOT_DIR.mkdirs();
+		while(new File(SCREENSHOT_DIR, screenshotIndex + ".png").exists()) ++screenshotIndex;
+		System.out.println("Starting at screenshot index " + screenshotIndex);
 	}
 	public static File saveScreenshot(Image img) throws SlickException{
-		File screenshot;
+		File screenshot = new File(SCREENSHOT_DIR, screenshotIndex++ + ".png");
 		
-		int i = 0;
-		do{
-			screenshot = new File(SCREENSHOT_DIR, i++ + ".png");
-		}while(screenshot.exists());
-		ImageOut.write(img, screenshot.toString());
+		try{
+			screenshot.createNewFile();
+		}catch(IOException ex){
+			throw new SlickException("Error creating screenshot file " + screenshot, ex);
+		}
+		try(FileOutputStream out = new FileOutputStream(screenshot)){
+			ImageOut.write(img, "PNG", out, true);
+		}catch(IOException ex){
+			throw new SlickException("Error taking screenshot " + screenshot, ex);
+		}
 		return screenshot;
 	}
 }
